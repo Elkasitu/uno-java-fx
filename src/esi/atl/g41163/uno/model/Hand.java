8d4512a8137205e7e67494e2a03485fab56aa107
Adrian Torres <41163@heb.be>
2017-03-04 20:36:22 +0100

    Add Hand class, missing javadoc for Discarded class' constructor

--- src/esi/atl/g41163/uno/model/Hand.java
+++ src/esi/atl/g41163/uno/model/Hand.java
@@ -7,27 +7,18 @@
 package esi.atl.g41163.uno.model;
 
 /**
- * A class representing the discarded cards pile in a game of UNO
+ * A class representing the player's hand
  * @author Adrian Torres
  * @version 1.0
  * @since 2017-03-04
  */
-public class Discarded extends CardCollection
+public class Hand
 {
     /**
      * This constructor calls CardCollection's constructor to instantiate itself
      */
-    Discarded()
+    Hand()
     {
         super();
-    }
-    
-    /**
-     * Gets the card at the top of the Discarded stack
-     * @return The card object at position 0
-     */
-    Card getCard()
-    {
-        return getCard(0);
     }
 }
